bb08d1d6bc07d1605a25e1f025592dbc3e349211
Ian McBurnie <ianmcburnie@hotmail.com>
2017-09-06 15:56:20 -0700

    Updated version number in source file

--- jquery.keyboardtrap.js
+++ jquery.keyboardtrap.js
@@ -1,7 +1,7 @@
 /**
 * @file jQuery singleton traps keyboard focus cycle within given element's interactive children
 * @author Ian McBurnie <ianmcburnie@hotmail.com>
-* @version 0.4.0
+* @version 1.0.0
 * @requires jquery
 * @requires jquery-focusable
 */
